120f140b2db6ad72e422aad14b4550f6c61b15c8
Diaz6620 <josem.diaz6620@gmail.com>
2025-01-06 22:04:48 +0100

    finish

--- src/weather/CitySelector.jsx
+++ src/weather/CitySelector.jsx
@@ -7,7 +7,7 @@ import styles from './CitySelector.module.css'
 const CitySelector = ({ selectedCity, onCityChange }) => {
     return (
         <div>
-            <label htmlFor="city-select">Selecciona una provincia: </label>
+            <label htmlFor="city-select" className={styles.label}>Selecciona una provincia: </label>
             <select
                 id="city-select"
                 value={selectedCity}
